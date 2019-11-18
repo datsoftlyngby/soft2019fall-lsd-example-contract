d7a0212d144c71a1c8690f7c75cbafbbdc0b81b5
eguahlak <anders@kalhauge.dk>
2019-11-18 14:17:50 +0100

    tests

--- src/test/java/dk/cphbusiness/example/contract/Holder.java
+++ src/test/java/dk/cphbusiness/example/contract/Holder.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dk.cphbusiness.example.contract;
 
-/**
- *
- * @author AKA
- */
 public class Holder {
   public static PersonManager manager;
-}
+  }
--- src/test/java/dk/cphbusiness/example/contract/PersonManagerTest.java
+++ src/test/java/dk/cphbusiness/example/contract/PersonManagerTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dk.cphbusiness.example.contract;
 
 import dk.cphbusiness.example.contract.dto.PersonSummary;
@@ -10,20 +5,12 @@ import java.util.Collection;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- *
- * @author AKA
- */
 public class PersonManagerTest {
-  
-  public PersonManagerTest() {
-  }
   
   @Test 
   public void testNumberOfPeople() {
     Collection<PersonSummary> people = Holder.manager.list();
-    assertEquals(people.size(), 3);
-            
+    assertEquals(3, people.size());
     }
   
-}
+  }
